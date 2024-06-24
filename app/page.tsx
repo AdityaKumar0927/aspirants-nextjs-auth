b29fc92f9e7ffa35f9300e6f8eb8f31a8cd396f2
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-06-24 09:49:44 -0500

    QuestionBank Removal

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default async function Home() {
         >
           <Link
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-300 bg-blue-400 px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href="/QuestionBank"
+            href="DEPLOY_URL"
           >
             <svg
               className="h-4 w-4 group-hover:text-black"
