0615f6b5e665bc0877b4687371b3a4f6d11b0dde
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-06-28 15:05:29 -0500

    HTML fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 };
               `,
             }}
-          /></head>
+          /><link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMGd8V0ER0VgLRW3UppZWW1tBgFO7VVHAb7FZk5"
+          crossOrigin="anonymous"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
+                link.integrity = 'sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMGd8V0ER0VgLRW3UppZWW1tBgFO7VVHAb7FZk5';
+                link.crossOrigin = 'anonymous';
+                document.head.appendChild(link);
+              })();
+            `,
+          }}
+        /></head>
       <body className={cx(sfPro.variable, inter.variable, "bg-gradient-to-br from-indigo-50 via-white to-cyan-100")}>
         <div className="fixed inset-0 z-[-10]"></div>
         <Suspense fallback="...">
