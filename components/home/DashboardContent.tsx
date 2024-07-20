cd95bb811de07ce72a828ac1a7ee5284a99f5b58
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-07-20 12:50:18 -0500

    ' escaped

--- components/home/DashboardContent.tsx
+++ components/home/DashboardContent.tsx
@@ -240,7 +240,7 @@ export default function Dashboard() {
           <CardHeader>
             <CardTitle>Progress</CardTitle>
             <CardDescription>
-              You're averaging better accuracy this year compared to last year.
+              You&apos;re averaging better accuracy this year compared to last year.
             </CardDescription>
           </CardHeader>
           <CardContent className="grid gap-4">
@@ -336,7 +336,7 @@ export default function Dashboard() {
           <CardHeader className="p-4 pb-0">
             <CardTitle>Time per Subtopic</CardTitle>
             <CardDescription>
-              Over the last 7 days, you've spent an average of 1.5 hours per subtopic per day.
+              Over the last 7 days, you&apos;ve spent an average of 1.5 hours per subtopic per day.
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-0">
@@ -564,7 +564,7 @@ export default function Dashboard() {
           <CardHeader className="p-4 pb-0">
             <CardTitle>Active Learning</CardTitle>
             <CardDescription>
-              You're maintaining an average accuracy of 85%. Good job!
+              You&apos;re maintaining an average accuracy of 85%. Good job!
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-2">
