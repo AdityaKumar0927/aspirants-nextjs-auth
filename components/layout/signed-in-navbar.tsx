2fb48ed70fdc3c74e3a1c6234a62628b47e71afc
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-08-12 06:55:20 -0500

    fix

--- components/layout/signed-in-navbar.tsx
+++ components/layout/signed-in-navbar.tsx
@@ -91,15 +91,15 @@ export default function NavBar({ session }: { session: Session | null }) {
     <>
       <SignInModal />
       <div
-        className={`fixed top-4 left-1/2 transform -translate-x-1/2 w-full max-w-screen-md bg-white ${
+        className={`fixed left-1/2 transform -translate-x-1/2 w-full ${
           scrolled
-            ? "bg-white/50 backdrop-blur-xl shadow-md rounded-3xl border-2"
-            : "bg-white/90 border- rounded-3xl"
+            ? "bg-white/50 backdrop-blur-xl"
+            : "bg-white/90"
         } flex justify-between items-center z-30 transition-all duration-300 ease-in-out`}
       >
         <div className="mx-5 flex h-16 items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
-            <p className="text-left font-display text-2xl font-bold tracking-[-0.02em] drop-shadow-sm sm:text-3xl sm:leading-[4rem]">
+            <p className="text-left font-display text-2xl tracking-[-0.02em] drop-shadow-sm sm:text-3xl sm:leading-[4rem]">
               aspirants
             </p>
             <Image
