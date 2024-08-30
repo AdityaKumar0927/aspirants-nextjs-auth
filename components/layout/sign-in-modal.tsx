f16db2c1a5dcdc5058ddfcaebce5c88eabfacf19
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-08-30 14:00:34 -0500

    fix

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -5,6 +5,7 @@ import { useState, Dispatch, SetStateAction, useCallback, useMemo } from "react"
 import { LoadingDots, Google } from "@/components/shared/icons";
 import Image from "next/image";
 import { Badge } from "../magicui/badge";
+import FlickeringGrid from "../magicui/flickering-grid";
 import { Button } from "@/app/Contact/button";
 import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
@@ -272,8 +273,20 @@ const SignInModal = ({
     
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
         <section
+        
       className="flex w-full items-start justify-center bg-[url('https://tailframes.com/images/squares-bg.webp')] bg-cover bg-center bg-no-repeat"
->
+>    
+      <FlickeringGrid
+        className="z-0 absolute inset-0 [mask:radial-gradient(circle_at_center,#fff_300px,transparent_0)]"
+        squareSize={4}
+        gridGap={6}
+        color="#60A5FA"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={800}
+        width={800}
+      />
+    
       <div
         className="flex max-w-screen-2xl grow flex-col items-start justify-start gap-12 px-3 py-12 md:pt-24 lg:px-0 xl:flex-row"
       >
@@ -283,18 +296,18 @@ const SignInModal = ({
           </Badge>
           <div className="flex max-w-lg flex-col gap-6">
             <h3 className="text-4xl font-semibold text-slate-950 md:text-6xl">
-              Here is your new Tailwind UI kit, welcome!
+              Start of your <div className="text-blue-300">Academic Comeback </div> with Aspirants!
             </h3>
             <h4 className="text-lg font-normal leading-7 text-slate-500">
-              We've done it carefully and simply. The elements work well together to create stunning landings.
+              And its free. Forever. 
             </h4>
           </div>
           <div className="flex gap-4">
             <Button>
-              Get a Template
+              Save Time
             </Button>
             <Button>
-              See More
+              Make Your Own Question Banks in 1-click
             </Button>
           </div>
         </div>
