8e0cfc9e10f5511b2e50cd3519064372b7502785
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-10-27 16:47:49 -0500

    Hero Section Font Change

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -109,7 +109,7 @@ const Page = () => {
             </AnimatedGradientText>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+              <h1 className="text-balance lg:text-5xl font-display tracking-[-0.07em] drop-shadow-sm sm:text-3xl sm:leading-[4rem]">
                  Study Smart With Aspirants
               </h1>
               <p
