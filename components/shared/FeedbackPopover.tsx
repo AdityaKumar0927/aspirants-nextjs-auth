8709d5b8e3ea68ae8fa00d01d818ab1bf887a4cd
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2024-11-23 07:28:38 -0600

    Update

--- components/shared/FeedbackPopover.tsx
+++ components/shared/FeedbackPopover.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@radix-ui/react-checkbox"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
