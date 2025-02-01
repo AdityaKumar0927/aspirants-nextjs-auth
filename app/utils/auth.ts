dda33cd916c6067af17d525432e4d5371e368270
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2025-01-31 20:30:19 -0600

    Update

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,28 +1,27 @@
 // utils/auth.ts
-import { NextApiRequest } from 'next';
-import { getSession } from 'next-auth/react';
-import { PrismaClient } from '@prisma/client';
+import { NextApiRequest } from 'next'
+import { getSession } from 'next-auth/react'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 // Authorization check function
 export const checkAuthorization = async (
   req: NextApiRequest,
   allowedRoles: string[]
 ): Promise<boolean> => {
-  const session = await getSession({ req });
+  const session = await getSession({ req })
 
-  // Check if session or user email is missing, return false
   if (!session || !session.user?.email) {
-    return false;
+    return false
   }
 
-  // Fetch the user and their role from the database
+  // Use UserRole instead of role
   const user = await prisma.user.findUnique({
     where: { email: session.user.email },
-    include: { role: true },
-  });
+    include: { UserRole: true },
+  })
 
-  // Check if user exists and has a role that is allowed
-  return Boolean(user && allowedRoles.includes(user.role?.name || ''));
-};
+  // Check if user exists and if user.UserRole?.name is in the allowed roles
+  return Boolean(user && allowedRoles.includes(user.UserRole?.name || ''))
+}
