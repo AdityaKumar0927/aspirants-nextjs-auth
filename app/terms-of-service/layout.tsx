12dc52698064190cfa84182818b389e3bcefeaf7
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2025-02-02 01:55:05 -0600

    Update

--- app/terms-of-service/layout.tsx
+++ app/terms-of-service/layout.tsx
@@ -78,7 +78,11 @@ export default async function PublicLayout({ children }: { children: React.React
         />
       </head>
       <body className={cx(sfPro.variable, inter.variable, 'bg-white dark:bg-dark-background text-black dark:text-white')}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider
+  attribute="class"
+  defaultTheme="light"
+  enableSystem={false}
+>
           <LoadingProvider>
             <UserPerformanceProvider userId={userId}>
               <TooltipProvider>
